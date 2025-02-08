2b4e0e2afd35331b2cb7a9d71011c11c28cdffd3
Ross Duquette <ross.duquette@gastronomous.ca>
2025-02-08 11:44:49 -0500

    Demonstrate sending empty messages

--- speaker.py
+++ speaker.py
@@ -3,9 +3,9 @@ from pubsub_python.publisher import Publisher
 from time import sleep
 
 pub_test = Publisher("test")
-pub_test2 = Publisher("test2")
+pub_empty = Publisher("empty")
 
 for i in range(10):
     pub_test.send_string(f"From speaker test: {i}")
-    pub_test2.send_string(f"From speaker test2: {i}")
+    pub_empty.send_empty()
     sleep(0.1)
